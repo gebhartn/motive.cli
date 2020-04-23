c3bb7aad32a4e886ae8c61cdd340a2b2b9030c6c
gebhartn <nicholas.gebhart@gmail.com>
2020-04-23 15:52:57 -0700

    chore: exports handlers

---
+++ src/cmds/index.ts
@@ -0,0 +1,11 @@
+import handleGet from './get'
+import handleDelete from './del'
+import handleAdd from './add'
+import handleDefault from './default'
+
+export const Commands = {
+  handleGet,
+  handleDelete,
+  handleAdd,
+  handleDefault,
+}
