0c508109b247c2bcac698a6f4a22de8d48c6e59b
gebhartn <nicholas.gebhart@gmail.com>
2020-04-23 15:51:56 -0700

    feat: todos services

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -1,23 +1,22 @@
-import { AxiosResponse } from 'axios'
 import { Auth } from '../utils'
 import login from './login'
 
 export const Todo = {
-  getAll: async (): Promise<AxiosResponse> => {
+  getAll: async (): Promise<any> => {
     const { payload } = await login()
     const { data } = await Auth.protectedRequest(payload).get('/todo')
 
     return data
   },
 
-  getOne: async (id: string): Promise<AxiosResponse> => {
+  getOne: async (id: string): Promise<any> => {
     const { payload } = await login()
     const { data } = await Auth.protectedRequest(payload).get(`/todo/${id}`)
 
     return data
   },
 
-  getBy: async (filter: string): Promise<AxiosResponse> => {
+  getBy: async (filter: string): Promise<any> => {
     const where = `/todo/filter?search=${filter}`
 
     const { payload } = await login()
@@ -26,17 +25,17 @@ export const Todo = {
     return data
   },
 
-  deleteOne: async (id: string): Promise<AxiosResponse> => {
+  deleteOne: async (id: string): Promise<any> => {
     const { payload } = await login()
     const { data } = await Auth.protectedRequest(payload).delete(`/todo/${id}`)
 
     return data
   },
 
-  makeOne: async (topic: string, content: string): Promise<AxiosResponse> => {
+  makeOne: async (category: string, content: string): Promise<any> => {
     const { payload } = await login()
     const { data } = await Auth.protectedRequest(payload).post(`/todo`, {
-      topic,
+      category,
       content,
     })
 
