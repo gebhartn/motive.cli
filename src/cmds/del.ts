5d0c79d021ddf553db33ac35cc3b3a6031ab257f
gebhartn <nicholas.gebhart@gmail.com>
2020-04-23 15:52:41 -0700

    feat: delete handler

---
+++ src/cmds/del.ts
@@ -0,0 +1,28 @@
+import { Todo } from '../services'
+import { Colors } from '../utils'
+
+import { CommandModule } from 'yargs'
+
+const command = 'done <id>'
+const describe = 'Deletes a todo by id'
+
+const builder = (yargs: any) => {
+  yargs.positional('id', {
+    describe: 'ID of the todo to be removed',
+    type: 'string',
+    demandOption: true,
+  })
+}
+
+const handler = (argv: any) => {
+  Todo.deleteOne(argv.id).then(() =>
+    Colors.printError(`Deleted todo with id: ${argv.id}`)
+  )
+}
+
+export default {
+  command,
+  describe,
+  builder,
+  handler,
+} as CommandModule
