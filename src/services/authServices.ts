b3d51cf613c20465e0b8a81a1dfd7f834002dd5d
gebhartn <nicholas.gebhart@gmail.com>
2020-04-22 22:08:54 -0700

    chore: authentication services

---
+++ src/services/authServices.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { Auth, Credentials } from './types'
+
+type AxiosPost = (endpoint: string) => (body: Credentials) => Promise<Auth>
+
+const authenticationPost: AxiosPost = endpoint => async body => {
+  const baseURL = process.env.BASE_URL || 'http://localhost:8000'
+
+  return axios
+    .post(baseURL + endpoint, body)
+    .then(response => {
+      const { status, data } = response
+
+      return { status, data }
+    })
+    .catch(error => {
+      const { response } = error
+      const { status, data } = response
+
+      return { status, data }
+    })
+}
+
+export const loginRequest = authenticationPost('/auth/login')
+export const registerRequest = authenticationPost('/auth/register')
--- src/services/types.d.ts
+++ src/services/types.d.ts
@@ -2,3 +2,12 @@ export interface Credentials {
   username: string
   password: string
 }
+
+interface Auth {
+  status: number
+  data: AuthPayload
+}
+
+interface AuthPayload {
+  payload: string
+}
