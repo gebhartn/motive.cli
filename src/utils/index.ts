6f5864c8c544b60ec8170604bf440c6cf882e264
gebhartn <nicholas.gebhart@gmail.com>
2020-04-23 12:04:24 -0700

    chore: print colors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as Colors } from './colors'
 export { default as Config } from './config'
 export { default as Auth } from './auth'
 export { default as Credentials } from './credentials'
